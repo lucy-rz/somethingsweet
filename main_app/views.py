ee87ba59852e02d9b11599eb803bd3972d0f1d1c
Lucy Ruiz <lucyruizwa@gmail.com>
2024-04-27 16:23:33 -0700

    add home, about & base

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from .models import Candy
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+# def candies_index(request):
+#     candies = Candy.objects.all()
+#     return render(request, 'candies/index.html', {
+#         'candies': candies
+#     })
