9d092615061d7ee13b288b040cc4830da08c4737
Lucy Ruiz <lucyruizwa@gmail.com>
2024-04-29 15:51:02 -0700

    migrate class

--- main_app/models.py
+++ main_app/models.py
@@ -10,11 +10,27 @@ class Candy(models.Model):
     description = models.TextField(max_length=300)
     cost = models.IntegerField()
 
+    def __str__(self):
+        return f'{self.name} {self.id}'
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    candies = models.ManyToManyField(Candy, through='OrderItems')
+    candies = models.ManyToManyField(Candy, through='OrderItem')
+    current_order = models.BooleanField(default=True)
 
-class OrderItems(models.Model):
-    order = models.ForeignKey(Order)
-    candy = models.ForeignKey(Candy)
+    def __str__(self):
+        return f'{self.user_id} {self.current_order}'
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    candy = models.ForeignKey(Candy, on_delete=models.CASCADE)
     quantity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.order_id} {self.candy_id} {self.quantity}'
+    
+class Photo(models.Model):
+    url = models.CharField(max_length=300)
+    candy = models.ForeignKey(Candy, on_delete=models.CASCADE)
+    def __str__(self):
+      return f'{self.candy_id} @{self.url}'
