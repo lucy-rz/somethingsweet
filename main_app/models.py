86ad584ca687579ad772cf97257f466687f04308
Lucy Ruiz <lucyruizwa@gmail.com>
2024-04-29 14:01:16 -0700

    define models

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Candy(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    description = models.TextField(max_length=300)
+    cost = models.IntegerField()
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    candies = models.ManyToManyField(Candy, through='OrderItems')
+
+class OrderItems(models.Model):
+    order = models.ForeignKey(Order)
+    candy = models.ForeignKey(Candy)
+    quantity = models.IntegerField()
