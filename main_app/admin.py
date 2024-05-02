6d4db04fab187cabfa397637ec05bd02601ca3c2
Lucy Ruiz <lucyruizwa@gmail.com>
2024-05-02 06:23:45 -0700

    update admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Candy, Order, OrderItem, Photo
 
 # Register your models here.
+admin.site.register(Candy)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Photo)
